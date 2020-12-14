ae4758769cf75998cf8152f42a416cc9618c8707
765947965 <765947965@qq.com>
2020-12-14 08:50:03 +0800

    自动播放

--- app/src/main/java/com/xwl/mvvm/business/banner/BannerActivity.kt
+++ app/src/main/java/com/xwl/mvvm/business/banner/BannerActivity.kt
@@ -6,17 +6,15 @@ import android.view.ViewGroup
 import androidx.appcompat.widget.AppCompatImageView
 import androidx.databinding.library.baseAdapters.BR
 import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import androidx.viewpager2.widget.ViewPager2
 import com.bumptech.glide.Glide
 import com.shuyu.gsyvideoplayer.GSYVideoManager
 import com.shuyu.gsyvideoplayer.video.StandardGSYVideoPlayer
 import com.shuyu.gsyvideoplayer.video.base.GSYVideoView
 import com.xwl.mvvm.R
 import com.xwl.mvvm.base.mvvm.BusinessBaseActivity
-import com.xwl.mvvm.business.banner.weight.NumIndicator
 import com.xwl.mvvm.databinding.BannerActivityBinding
-import com.youth.banner.adapter.BannerAdapter
-import com.youth.banner.config.IndicatorConfig
-import com.youth.banner.listener.OnPageChangeListener
 import kotlinx.android.synthetic.main.banner_activity.*
 
 /**
@@ -36,71 +34,69 @@ class BannerActivity : BusinessBaseActivity<BannerModel, BannerActivityBinding, 
 
     override fun initView() {
         val data = viewModel.getData()
-        banner.addBannerLifecycleObserver(this)
-                .setIndicator(NumIndicator(this))
-                .setIndicatorGravity(IndicatorConfig.Direction.RIGHT)
-                .setAdapter(object : BannerAdapter<String, RecyclerView.ViewHolder>(data) {
-                    override fun getItemViewType(position: Int): Int {
-                        return if (data[position].endsWith(".mp4")) 1 else 0
+        banner.adapter = object : RecyclerView.Adapter<ViewHolder>() {
+            override fun getItemViewType(position: Int): Int {
+                return if (data[position].endsWith(".mp4")) 1 else 0
+            }
+
+            override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+                val view = if (1 == viewType) {
+                    LayoutInflater.from(this@BannerActivity).inflate(R.layout.video_item, parent, false)
+                } else {
+                    LayoutInflater.from(this@BannerActivity).inflate(R.layout.image_item, parent, false)
+                }
+                view.tag = viewType
+                return object : RecyclerView.ViewHolder(view) {}
+            }
+
+            override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+                val viewType = getItemViewType(position)
+                holder.itemView.tag = position
+                holder.itemView.setTag(R.id.itemRoot, viewType)
+                if (1 == viewType) {
+                    holder.itemView.findViewById<StandardGSYVideoPlayer>(R.id.player)?.run {
+                        isLooping = true
+                        setUp(data[position], true, context.cacheDir, null)
+                        backButton.visibility = View.GONE
                     }
-
-                    override fun onCreateHolder(parent: ViewGroup?, viewType: Int): RecyclerView.ViewHolder {
-                        val view = if (1 == viewType) {
-                            LayoutInflater.from(this@BannerActivity).inflate(R.layout.video_item, parent, false)
-                        } else {
-                            LayoutInflater.from(this@BannerActivity).inflate(R.layout.image_item, parent, false)
-                        }
-                        return object : RecyclerView.ViewHolder(view) {}
+                } else {
+                    holder.itemView.findViewById<AppCompatImageView>(R.id.image)?.run {
+                        Glide.with(this).load(data[position]).centerCrop().into(this)
                     }
+                }
+            }
 
-                    override fun onBindView(holder: RecyclerView.ViewHolder?, data: String?, position: Int, size: Int) {
-                        if (1 == getItemViewType(position)) {
-                            holder?.itemView?.findViewById<StandardGSYVideoPlayer>(R.id.player)?.run {
-                                isLooping = true
-                                setUp(data, true, context.cacheDir, null)
-                                backButton.visibility = View.GONE
-                            }
-                        } else {
-                            holder?.itemView?.findViewById<AppCompatImageView>(R.id.image)?.run {
-                                Glide.with(this).load(data).centerCrop().into(this)
-                            }
-                        }
-                    }
-                }, false)
-                .addOnPageChangeListener(object : OnPageChangeListener {
-                    override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
-
-                    }
-
-                    override fun onPageSelected(position: Int) {
-                        player?.onVideoPause()
-                        if (data[position].endsWith(".mp4")) {
-                            // 视频
-                            player = banner.adapter.viewHolder.itemView.findViewById<StandardGSYVideoPlayer>(R.id.player)
-                            player?.run {
-                                if (currentState == GSYVideoView.CURRENT_STATE_PAUSE) {
-                                    onVideoResume()
-                                } else {
-                                    startPlayLogic()
-                                }
-                            }
-                        } else {
-                            player = null
-                        }
-                    }
-
-                    override fun onPageScrollStateChanged(state: Int) {
-
-                    }
-
-                })
-        banner.post {
-            if (data[0].endsWith(".mp4")) {
-                banner.adapter.viewHolder.itemView.findViewById<StandardGSYVideoPlayer>(R.id.player)?.startPlayLogic()
+            override fun getItemCount(): Int {
+                return data.size
             }
         }
-        banner.isAutoLoop(true)
-        banner.setLoopTime(10000)
+        banner.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                player?.onVideoPause()
+                banner.findViewWithTag<View>(position)?.let { itemRoot ->
+                    val viewType = itemRoot.getTag(R.id.itemRoot) as Int
+                    if (viewType == 1) {
+                        player = itemRoot.findViewById(R.id.player)
+                        player?.run {
+                            if (currentState == GSYVideoView.CURRENT_STATE_PAUSE) {
+                                onVideoResume()
+                            } else {
+                                startPlayLogic()
+                            }
+                        }
+                    } else {
+                        player = null
+                    }
+                }
+            }
+        })
+        banner.offscreenPageLimit = data.size
+        banner.post {
+            if (data[0].endsWith(".mp4")) {
+                banner.findViewWithTag<View>(0).findViewById<StandardGSYVideoPlayer>(R.id.player)?.startPlayLogic()
+            }
+        }
     }
 
     override fun initData() {}
